2d450acc78bce5f139e8ba02a0bfe47ea9b5a98b
Dennis <denkipyator@gmail.com>
2025-01-13 00:05:00 +0300

    Orders page done

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useContext } from 'react'
+import Login from './pages/login'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AdminContext } from './context/AdminContext';
+import { Route, Routes  } from 'react-router';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Admin/Dashboard.jsx';
+import AllAppointments from './pages/Admin/AllAppointments.jsx';
+import AddDoctor from './pages/Admin/AddDoctor.jsx';
+import DoctorList from './pages/Admin/DoctorsList.jsx'
+import { DoctorContext } from './context/DoctorContext.jsx';
+import DoctorDashboard from './pages/Doctor/DoctorDashboard.jsx';
+import DoctorAppointments from './pages/Doctor/DoctorAppointments.jsx';
+import DoctorProfile from './pages/Doctor/DoctorProfile.jsx';
+import AddMedicine from './pages/Admin/AddMedicine.jsx';
+import ListMedicine from './pages/Admin/ListMedicine.jsx';
+import Orders from './pages/Admin/Orders.jsx';
+
+const App = () => {
+
+  const {aToken} = useContext(AdminContext)
+  const {dToken} = useContext(DoctorContext)
+
+  return aToken || dToken ? (
+    <div className='bg-[#F8F9FD] min-h-screen'>
+      <ToastContainer/>
+      <Navbar/>
+      <div className='flex items-start w-full'>
+        <Sidebar/>
+        <Routes>
+          {/* Admin Route */}
+          <Route path='/' element={<></>}/>
+          <Route path='/admin-dashboard' element={<Dashboard/>}/>
+          <Route path='/all-appointments' element={<AllAppointments/>}/>
+          <Route path='/add-doctor' element={<AddDoctor/>}/>
+          <Route path='/doctor-list' element={<DoctorList/>}/>
+          <Route path='/add-medicine' element={<AddMedicine/>}/>
+          <Route path='/medicine-list' element={<ListMedicine/>}/>
+          <Route path='/orders' element={<Orders/>}/>
+
+          {/* Doctor Route */}
+          <Route path='/doctor-dashboard' element={<DoctorDashboard/>}/>
+          <Route path='/doctor-appointments' element={<DoctorAppointments/>}/>
+          <Route path='/doctor-profile' element={<DoctorProfile/>}/>
+        </Routes>
+      </div>
+    </div>
+  ) : (
+    <div>
+      <Login/>
+      <ToastContainer/>
+    </div>
+  )
+}
+
+export default App
